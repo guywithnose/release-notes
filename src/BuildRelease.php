6fcaff159bb0e94ca6df5291582d4110664a6216
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-05-29 07:05:19 -0400

    Make final question easier to understand.
    The wording of the question was confusing.  I believe a simple
    "Continue?" will be easier to follow.

--- src/BuildRelease.php
+++ src/BuildRelease.php
@@ -325,13 +325,7 @@ class BuildRelease extends Command
         $lines = array_merge([$release['name'], ''], explode("\n", $release['body']));
         $formattedNotes = $formatter->formatBlock($lines, 'info', true);
 
-        if (
-            $dialog->askConfirmation(
-                $output,
-                "{$formattedNotes}\n<question>Publish this release as a draft?</question> <info>(default: yes)</info> ",
-                true
-            )
-        ) {
+        if ($dialog->askConfirmation($output, "{$formattedNotes}\n<question>Continue?</question> <info>(default: yes)</info> ", true)) {
             $client->createRelease($release);
         }
     }
