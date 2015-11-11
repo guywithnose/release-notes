f1aa1bc3a4da1154a346bab2ef5c58f3806a3166
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2015-11-10 23:00:21 -0500

    Resolves #17.
    Removed page-level doc blocks.

---
+++ tests/src/Change/ChangeListFactoryTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace Guywithnose\ReleaseNotes\Tests\Change;
+
+use Guywithnose\ReleaseNotes\Change\Change;
+use Guywithnose\ReleaseNotes\Change\ChangeFactory;
+use Guywithnose\ReleaseNotes\Change\ChangeList;
+use Guywithnose\ReleaseNotes\Change\ChangeListFactory;
+
+class ChangeListFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreateFromCommits()
+    {
+        $changeFactory = new ChangeFactory();
+        $commit = json_decode(file_get_contents('tests/data/commit.json'), true);
+        $expectedChangeList = new ChangeList([new Change($commit)]);
+        $changeListFactory = new ChangeListFactory($changeFactory);
+        $actualChangeList = $changeListFactory->createFromCommits([$commit]);
+        $this->assertEquals($expectedChangeList, $actualChangeList);
+    }
+}
